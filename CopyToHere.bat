fd1b839e50445f52589ccf85a558eb04d53cf932
Minghan <bluefoxxu@gmail.com>
2019-04-14 03:00:33 -0400

    Auto 2019-04-14 3:00:33.41

--- CopyToHere.bat
+++ CopyToHere.bat
@@ -1,8 +1,14 @@
-
+@echo off
+setlocal EnableDelayedExpansion
+git pull origin master
 set DIR="%D:\workspace\Leetcode-workplace\%"
-for /r %DIR% %%i in (Solution.java) do (
-	set str1=%%~pdi
-	set str2=%str1:~32%
-	set str3=%str2:~0,-5%
-	copy %%i "%str3%.java"
+for /r %DIR% %%i in (*.java) do (
+	set str=%%~pdi
+	set str=!str:~32!
+	set str=!str:~0,-5!
+	copy %%i !str!.java
 )
+echo %date%%time%
+git add .
+git commit -m "Auto %date%%time%"
+pause

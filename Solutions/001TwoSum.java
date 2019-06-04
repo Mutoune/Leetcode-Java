49222f4334110e45c89f7602b75cb3e32edee196
Minghan <bluefoxxu@gmail.com>
2019-06-04 10:06:04 -0400

    Auto Update@2019-06-04 10:06:04.78

--- Solutions/001TwoSum.java
+++ Solutions/001TwoSum.java
@@ -1,19 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
-    public int[] twoSum(int[] nums, int target) {
-        int[] rtn = new int[2];
-        int size = nums.length;
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = i + 1; j < size; j++) {
-                if (nums[i] + nums[j] == target) {
-                    rtn[0] = i;
-                    rtn[1] = j;
-                    break;
-                }
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] rtn = new int[2];
+//        int size = nums.length;
+//        for (int i = 0; i < size - 1; i++) {
+//            for (int j = i + 1; j < size; j++) {
+//                if (nums[i] + nums[j] == target) {
+////                    rtn[0] = i;
+////                    rtn[1] = j;
+////                    break;
+//                    return new int[] {i, j};
+//                }
+//            }
+//        }
+//        return rtn;
+//    }
+
+    public int[] twoSum(int[] array, int target) throws IllegalArgumentException {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int complement = target - array[i];
+            if (map.containsKey(complement)) {
+                return new int[]{ map.get(complement), i };
             }
+            map.put(array[i], i);
         }
-        return rtn;
+        throw new IllegalArgumentException("No two sum solution");
     }
 
     public static void main(String[] args) {
---
+++ Solutions/198HouseRobber.java
@@ -0,0 +1,14 @@
+
+public class Solution {
+
+    public int rob(int[] nums) {
+        int amount = 0;
+        
+        return amount;
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}

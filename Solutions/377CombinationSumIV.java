f1f1d54d0e71e60e61a8794839b0198c740a163f
Minghan <bluefoxxu@gmail.com>
2019-06-03 23:05:26 -0400

    Auto Update@2019-06-03 23:05:25.95

---
+++ Solutions/377CombinationSumIV.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] rtn = new int[target + 1];
+        Arrays.sort(nums);
+        rtn[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > target) {
+                break;
+            }
+            rtn[nums[i]] = 1;
+        }
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    rtn[i] += rtn[i - nums[j]];
+                } else break;
+            }
+        }
+        return rtn[target];
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 2, 3};
+        int target = 4;
+        System.out.println(new Solution().combinationSum4(nums, target));
+    }
+
+}

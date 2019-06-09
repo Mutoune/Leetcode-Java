a1f53487e37f1df0cd8fdc0c1c6ee68e8d722185
Minghan <bluefoxxu@gmail.com>
2019-06-09 17:28:30 -0400

    Auto Update@2019-06-09 17:28:30.17

---
+++ Solutions/0100SameTree.java
@@ -0,0 +1,22 @@
+
+public class Solution {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        else if (p == null || q == null) return false;
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}

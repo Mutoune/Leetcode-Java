48922863273fddee90fba1e8e3e8ebbcee221595
Minghan <bluefoxxu@gmail.com>
2019-06-09 01:56:33 -0400

    Auto Update@2019-06-09  1:56:33.12

--- Solutions/094BinaryTreeInorderTraversal.java
+++ Solutions/094BinaryTreeInorderTraversal.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+//import java.util.Stack;
 
 public class Solution {
 
@@ -9,6 +10,24 @@ public class Solution {
         TreeNode right;
         TreeNode(int x) { val = x; }
     }
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> rtn = new ArrayList<>();
+//        if (root == null) return rtn;
+//        Stack<TreeNode> nodeList = new Stack<>();
+//        nodeList.add(root);
+//        while (nodeList.size() > 0) {
+//            TreeNode node = nodeList.peek();
+//            if (node.left == null) {
+//                rtn.add(node.val);
+//                nodeList.pop();
+//                if (node.right != null) nodeList.push(node.right);
+//            } else {
+//                nodeList.push(node.left);
+//                node.left = null;
+//            }
+//        }
+//        return rtn;
+//    }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> rtn = new ArrayList<>();
         if (root == null) return rtn;
@@ -27,6 +46,7 @@ public class Solution {
         }
         return rtn;
     }
+
     public static void main(String[] args) {
         TreeNode node3 = new TreeNode(3);
         TreeNode node2 = new TreeNode(2);

8da6c737bfbb563aee6cefcbb2c99412777c30cc
Minghan <bluefoxxu@gmail.com>
2019-06-09 12:31:36 -0400

    Auto Update@2019-06-09 12:31:35.99

--- Solutions/0069Sqrtx.java
+++ Solutions/0069Sqrtx.java
@@ -2,11 +2,20 @@
 public class Solution {
 
     public int mySqrt(int x) {
-        int i = 1;
-        while (x / i  >= i) {
-            i++;
+        int left = 1;
+        int right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (x / mid < mid) {
+                right = mid;
+            } else {
+                if (x / (mid + 1) < (mid + 1)) {
+                    return mid;
+                }
+                left = mid + 1;
+            }
         }
-        return i - 1;
+        return 0;
     }
     public static void main(String[] args) {
         System.out.println(new Solution().mySqrt(2147395600));

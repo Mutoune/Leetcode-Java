e6539c46456cd7029bdbf66eb44fac83d3f82e7f
Minghan <bluefoxxu@gmail.com>
2019-06-09 11:56:58 -0400

    Auto Update@2019-06-09 11:56:58.36

--- Solutions/0067AddBinary.java
+++ Solutions/0067AddBinary.java
@@ -2,31 +2,19 @@
 public class Solution {
 
     public String addBinary(String a, String b) {
-        int len = a.length() > b.length() ? a.length() : b.length();
-        int[] res = new int[len];
-        int lenb = b.length() - 1;
+        StringBuffer sb = new StringBuffer();
         int lena = a.length() - 1;
-        int lres = 0;
-        int flg = 0;
-        while (lenb >= 0 || lena >= 0) {
-            int temp = flg;
-            if (lenb >=0) temp += b.charAt(lenb) - '0';
-            if (lena >=0) temp += a.charAt(lena) - '0';
-            if (temp > 1) {
-                flg = 1;
-                temp = temp - 2;
-            } else flg = 0;
-            res[lres] = temp;
-            lres++;
-            lena--;
-            lenb--;
+        int lenb = b.length() - 1;
+        int carry = 0;
+        while (lena >=0 || lenb >= 0) {
+            int sum = carry;
+            if (lena >= 0) sum += a.charAt(lena--) - '0';
+            if (lenb >= 0) sum += b.charAt(lenb--) - '0';
+            carry = sum / 2;
+            sb.append(sum % 2);
         }
-        String str = "";
-        if (flg == 1) str += "1";
-        for (int i = len - 1; i >= 0; i--) {
-            str += res[i];
-        }
-        return str;
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {

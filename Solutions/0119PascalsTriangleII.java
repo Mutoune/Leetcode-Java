e6149aec30efed76fb57bc28393cf563c61552f4
Minghan <bluefoxxu@gmail.com>
2019-06-12 23:56:28 -0400

    Auto Update@2019-06-12 23:56:28.60

---
+++ Solutions/0119PascalsTriangleII.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        for (int i = 1; i < rowIndex + 1; i++) {
+            int temp = 1;
+            for (int j = 1; j < i + 1; j++) {
+                a[]
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}

c1755dfd4dfb9c849b19e0c86ae93fd5d1c0896e
Minghan <bluefoxxu@gmail.com>
2019-06-13 23:15:08 -0400

    Auto Update@2019-06-13 23:15:08.87

--- Solutions/0206ReverseLinkedList.java
+++ Solutions/0206ReverseLinkedList.java
@@ -10,19 +10,27 @@ public class Solution {
         }
     }
 
+//    public ListNode reverseList(ListNode head) {
+//        if (head == null || head.next == null) return head;
+//        Stack<ListNode> stack = new Stack<>();
+//        stack.add(head);
+//        while (stack.peek().next != null) {
+//            stack.push(stack.peek().next);
+//        }
+//        ListNode node = stack.peek();
+//        while (stack.size() > 1) {
+//            stack.pop().next = stack.peek();
+//        }
+//        stack.peek().next = null;
+//        return node;
+//    }
     public ListNode reverseList(ListNode head) {
-        if (head == null) return null;
-        Stack<ListNode> stack = new Stack<>();
-        stack.add(head);
-        while (stack.peek().next != null) {
-            stack.push(stack.peek().next);
-        }
-        ListNode node = stack.peek();
-        while (stack.size() > 1) {
-            stack.pop().next = stack.peek();
-        }
-        stack.peek().next = null;
-        return node;
+        if (head == null || head.next == null) return head;
+        ListNode node = head.next;
+        ListNode newHead = reverseList(node);
+        node.next = head;
+        head.next = null;
+        return newHead;
     }
     public static void main(String[] args) {
         // TODO Auto-generated method stub

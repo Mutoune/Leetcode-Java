5f19e5f1e684254e58a993021283d9ef462411f4
Minghan <bluefoxxu@gmail.com>
2019-06-22 00:03:54 -0400

    Auto Update@2019-06-22  0:03:54.62

--- Solutions/0017LetterCombinationsofaPhoneNumber.java
+++ Solutions/0017LetterCombinationsofaPhoneNumber.java
@@ -1,12 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class Solution {
 
     public List<String> letterCombinations(String digits) {
+        LinkedList<String> list = new LinkedList<>();
+        if (digits.length() == 0) return null;
         Map<Integer, char[]> map = new HashMap<>();
         map.put(2, new char[] {'a', 'b', 'c'});
         map.put(3, new char[] {'d', 'e', 'f'});
@@ -15,11 +16,21 @@ public class Solution {
         map.put(6, new char[] {'m', 'n', 'o'});
         map.put(7, new char[] {'p', 'q', 'r', 's'});
         map.put(8, new char[] {'t', 'u', 'v'});
-        map.put(2, new char[] {'w', 'x', 'y','z'});
+        map.put(9, new char[] {'w', 'x', 'y','z'});
+        list.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int target = Integer.parseInt(digits.substring(i, i + 1));
+            while (list.peek().length() == i) {
+                for (int k = 0; k < map.get(target).length; k++) {
+                    list.add(list.peek() + map.get(target)[k]);
+                }
+                list.poll();
+            }
+        }
+        return list;
     }
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        System.out.println(new Solution().letterCombinations("2334").toString());
     }
 
 }

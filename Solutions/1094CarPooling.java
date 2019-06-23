769189f90347019cd32be41d765fb6c7ac74b0c4
Minghan <bluefoxxu@gmail.com>
2019-06-23 00:15:50 -0400

    Auto Update@2019-06-23  0:15:50.08

---
+++ Solutions/1094CarPooling.java
@@ -0,0 +1,19 @@
+
+public class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] station = new int[1001];
+        for (int i = 0; i < trips.length; i++) {
+            for (int j = trips[i][1]; j < trips[i][2]; j++) {
+                station[j] += trips[i][0];
+                if (station[j] > capacity) return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[][] nums = new int[][] {{3,2,7},{3,7,9},{8,3,9}};
+        int target = 11;
+        System.out.println(new Solution().carPooling(nums, target));
+    }
+
+}

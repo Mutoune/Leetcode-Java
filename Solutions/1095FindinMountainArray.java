baba6d2c6a71b17d14768efa4047d6dfadd2da45
Minghan <bluefoxxu@gmail.com>
2019-06-23 11:28:13 -0400

    Auto Update@2019-06-23 11:28:13.25

--- Solutions/1095FindinMountainArray.java
+++ Solutions/1095FindinMountainArray.java
@@ -6,46 +6,33 @@ public class Solution {
     }
     public int findInMountainArray(int target, MountainArray mountainArr) {
         int n = mountainArr.length();
-        int top = -1;
-        {
-            int low = 0, high = n;
-            while(high - low > 1){
-                int h = high+low-1>>1;
-                if(mountainArr.get(h) < mountainArr.get(h+1)){
-                    low = h+1;
-                }else{
-                    high = h+1;
-                }
-            }
-            top = low;
+        int min = 0, max = n - 1;
+        while (max > min) {
+            int mid = (max - min) / 2 + min;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                min = mid + 1;
+            } else { max = mid;}
         }
-        if(mountainArr.get(top) == target)return top;
-        {
-            int low = 0, high = top+1;
-            while(high - low > 0){
-                int h = high+low>>1;
-                int v = mountainArr.get(h);
-                if(v == target)return h;
-                if(v < target){
-                    low = h+1;
-                }else{
-                    high = h;
-                }
-            }
+        int top = max;
+        System.out.println(top);
+        if (mountainArr.get(top) == target) return max;
+        min = 0;
+        while (max > min) {
+            int mid = (max - min) / 2 + min;
+            if (mountainArr.get(mid) < target) {
+                min = mid + 1;
+            } else { max = mid;}
         }
-        {
-            int low = top, high = n;
-            while(high - low > 0){
-                int h = high+low>>1;
-                int v = mountainArr.get(h);
-                if(v == target)return h;
-                if(v > target){
-                    low = h+1;
-                }else{
-                    high = h;
-                }
-            }
+        if (mountainArr.get(max) == target) return max;
+        min = top;
+        max = n - 1;
+        while (max > min) {
+            int mid = (max - min) / 2 + min;
+            if (mountainArr.get(mid) > target) {
+                min = mid + 1;
+            } else { max = mid;}
         }
+        if (mountainArr.get(max) == target) return max;
         return -1;
     }
     public static void main(String[] args) {

3a3accd6292f8929b5cb286c2b0cc853d46875a9
Minghan <bluefoxxu@gmail.com>
2019-06-28 23:52:55 -0400

    Auto Update@2019-06-28 23:52:55.73

--- Solutions/0051NQueens.java
+++ Solutions/0051NQueens.java
@@ -3,17 +3,11 @@ import java.util.List;
 
 public class Solution {
 
-    private void dfs
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> lists = new ArrayList<>();
-        int[] res = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            if dfs(lists, res, 1, n);
-        }
-        
         return lists;
     }
+    
     public static void main(String[] args) {
         
     }

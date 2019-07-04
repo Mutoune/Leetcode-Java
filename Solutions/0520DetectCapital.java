6598c3c0bef9303e206bc80c2d6884970e76ebb2
Minghan <bluefoxxu@gmail.com>
2019-07-03 22:51:13 -0400

    Auto Update@2019-07-03 22:51:12.96

---
+++ Solutions/0520DetectCapital.java
@@ -0,0 +1,10 @@
+
+public class Solution {
+
+    public boolean detectCapitalUse(String word) {
+        return word.matches("[A-Z]+|[a-z]+|[A-Z][a-z]+");
+    }
+    public static void main(String[] args) {
+        
+    }
+}
